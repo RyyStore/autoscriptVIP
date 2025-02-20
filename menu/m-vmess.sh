3250f41316fb376556b20b3510d4297f88967071
RyyNotavailable <48169572+ryankputra@users.noreply.github.com>
2025-02-20 22:29:09 +0700

    Update m-vmess.sh

--- menu/m-vmess.sh
+++ menu/m-vmess.sh
@@ -3,44 +3,54 @@
 # Mendapatkan tanggal dari server
 biji=$(date +"%Y-%m-%d" -d "$dateFromServer")
 
-# Menentukan tema warna
-colornow=$(cat /etc/rmbl/theme/color.conf)
-NC="\e[0m"
-RED="\033[0;31m"
-COLOR1="$(grep -w "TEXT" /etc/rmbl/theme/$colornow | cut -d: -f2 | sed 's/ //g')"
-COLBG1="$(grep -w "BG" /etc/rmbl/theme/$colornow | cut -d: -f2 | sed 's/ //g')"
-WH='\033[1;37m'
-
 # Mengambil IP VPS dengan metode alternatif
-ipsaya=$(curl -s https://api.ipify.org || curl -s https://checkip.amazonaws.com || curl -s https://icanhazip.com)
+MYIP=$(curl -s https://api.ipify.org || curl -s https://checkip.amazonaws.com || curl -s https://icanhazip.com)
 
-# Cek apakah IP berhasil diambil
-if [[ -z "$ipsaya" ]]; then
+# Cek apakah MYIP berhasil diambil
+if [[ -z "$MYIP" ]]; then
     echo -e "\033[0;31mGagal mendapatkan IP VPS!\033[0m"
     exit 1
 fi
 
-echo -e "\033[0;32mIP VPS: $ipsaya\033[0m"
+echo -e "\033[0;32mIP VPS: $MYIP\033[0m"
 
-# Mengambil tanggal server dari Google
-data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+# Mengatur warna
+colornow=$(cat /etc/rmbl/theme/color.conf)
+export NC="\e[0m"
+export yl='\033[0;33m'
+export RED="\033[0;31m"
+export COLOR1="$(cat /etc/rmbl/theme/$colornow | grep -w "TEXT" | cut -d: -f2 | sed 's/ //g')"
+export COLBG1="$(cat /etc/rmbl/theme/$colornow | grep -w "BG" | cut -d: -f2 | sed 's/ //g')"
+export WH='\033[1;37m'
 
-# Konversi ke format yang bisa dibaca
-if [[ -n "$data_server" ]]; then
-    date_list=$(date +"%Y-%m-%d" -d "$data_server")
-else
-    echo -e "\033[0;31mGagal mendapatkan tanggal dari server!\033[0m"
+# Mengambil informasi sistem
+tram=$(free -h | awk 'NR==2 {print $2}')
+uram=$(free -h | awk 'NR==2 {print $3}')
+ISP=$(cat /etc/xray/isp)
+CITY=$(cat /etc/xray/city)
+author=$(cat /etc/profil)
+DATE2=$(date -R | cut -d " " -f -5)
+
+# Sumber data izin
+data_ip="https://raw.githubusercontent.com/RyyStore/permission/main/ip"
+
+# Mengambil data izin berdasarkan IP
+Exp2=$(curl -sS "$data_ip" | grep -w "$MYIP" | awk '{print $3}')
+
+# Cek apakah Exp2 ditemukan
+if [[ -z "$Exp2" ]]; then
+    echo -e "\033[0;31mIP tidak ditemukan dalam daftar izin!\033[0m"
     exit 1
 fi
 
+echo -e "\033[0;32mExp2: $Exp2\033[0m"
+
+# Mendapatkan tanggal server dari Google
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+
 echo -e "\033[0;32mTanggal Server: $date_list\033[0m"
 
-# URL daftar IP izin
-data_ip="https://raw.githubusercontent.com/RyyStore/permission/main/ip"
-
-# Fungsi untuk memeriksa izin script
-checking_sc() {
-    useexp=$(curl -sS "$data_ip" | grep -w "$ipsaya" | awk '{print $3}')
 if [[ $date_list < $useexp ]]; then
 echo -ne
 else
